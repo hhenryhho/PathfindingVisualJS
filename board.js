bb6946f95357ff057d37bc08bf8cc5b4c4395544
Henry Ho <henryho73@hotmail.com>
2020-01-16 22:34:12 -0500

    Implemented BFS Pathfinding

--- board.js
+++ board.js
@@ -1,8 +1,9 @@
 function Board() {
     this.nodeArray = []
     this.startCoordinate = ''
-    this.endCordinate = ''
+    this.endCoordinate = ''
     this.nodeWall = []
+    this.nodeVisited = []
     this.isDragging = false
     this.isStartDragging = false
     this.isEndDragging = false
@@ -35,8 +36,8 @@ Board.prototype.createGrid = function(width, height) {
     $("#board").html(htmlGrid);
 
     // Set start and end nodes
-    this.setStart(Math.round(height/2), Math.round(width/4))
-    this.setEnd(Math.round(height/2), Math.round(width/4 * 3))
+    this.setSpecial(Math.round(height/2), Math.round(width/4), 'start')
+    this.setSpecial(Math.round(height/2), Math.round(width/4 * 3), 'end')
 }
 
 Board.prototype.addEventListeners = function() {
@@ -50,14 +51,17 @@ Board.prototype.addEventListeners = function() {
             node = document.getElementById(row + ',' + column)
             // Create an event listener for mouse down for each node
             $(node).mousedown(self, function() {
+                x = this.id.split(',')[0]
+                y = this.id.split(',')[1]
                 if ($(this).attr('class') === 'start'){
                     self.isStartDragging = true
                 } else if ($(this).attr('class') === 'end') {
                     self.isEndDragging = true
-                } else if ($(this).attr('class') === 'unvisited' || $(this).attr('class') === 'wall') { 
-                    self.changeUnvisitedToWall(this)
+                } else if ($(this).attr('class') === 'unvisited') { 
+                    self.setNormal(x, y, 'unvisited', 'wall')
                     self.isDragging = true
-                    console.log(self.isDragging)
+                } else if ($(this).attr('class') === 'wall') {
+                    self.setNormal(x, y, 'wall', 'unvisited')
                 }
             })
             // Create an event listener for mouse up
@@ -65,18 +69,22 @@ Board.prototype.addEventListeners = function() {
                 self.isDragging = false
                 self.isStartDragging = false
                 self.isEndDragging = false
-                console.log(self.isDragging)
+
             })
             // Create an event listener for mouse drag
             $(node).mouseenter(self, function() {
                 x = this.id.split(',')[0]
                 y = this.id.split(',')[1]
                 if (self.isStartDragging === true) {
-                    self.setStart(x, y)
+                    self.setSpecial(x, y, 'start')
                 } else if (self.isEndDragging === true) {
-                    self.setEnd(x, y)
+                    self.setSpecial(x, y, 'end')
                 } else if (self.isDragging === true){
-                    self.changeUnvisitedToWall(this)
+                    if (self.nodeArray[x][y].status === 'unvisited'){
+                        self.setNormal(x, y, 'unvisited', 'wall')
+                    } else if (self.nodeArray[x][y].status === 'wall') {
+                        self.setNormal(x, y, 'wall', 'unvisited')
+                    }
                 }   
             })
         }
@@ -90,111 +98,206 @@ Board.prototype.addEventListeners = function() {
     $('#reset').click(self, function(){
         self.clear()
         self.isReset = true
-        self.setStart(Math.round(height/2), Math.round(width/4))
-        self.setEnd(Math.round(height/2), Math.round(width/4 * 3))
+        self.setSpecial(Math.round(height/2), Math.round(width/4), 'start')
+        self.setSpecial(Math.round(height/2), Math.round(width/4 * 3), 'end')
         self.isReset = false
     })
 }
 
-Board.prototype.changeUnvisitedToWall = function(node) {
-    if ($(node).attr('class') === 'unvisited') {
-        $(node).addClass('wall').removeClass('unvisited');
+Board.prototype.setNormal = function(x, y, before, after) {
+    node = this.nodeArray[x][y]
+    htmlNode = document.getElementById(x + ',' + y)
+
+    if (before === 'unvisited' && after === 'wall') {
+        // Change the class name of the node in html to be a wall
+        htmlNode.className = 'wall'
+        // Change the status of the node in nodeArray to be a wall
+        node.status = 'wall'
+        // Add it to the nodeWall array
         this.nodeWall.push(node)
-    } else if ($(node).attr('class') === 'wall') {
-        for (currentNode in this.nodeWall) {
-            if (this.nodeWall[currentNode] === node) {
-                this.nodeWall.splice(currentNode, 1)
-                $(node).addClass('unvisited').removeClass('wall');
-            }
+    } else if (before === 'wall' && after === 'unvisited'){
+        // Find the node in nodeWall array
+        nodeInWallArray = this.nodeWall.indexOf(node)
+        // If the node exists
+        if (nodeInWallArray !== -1) {
+            // Change the class name of the node in html to be unvisited
+            htmlNode.className = 'unvisited'
+            // Change the status of the node in nodeArray to be unvisited
+            node.status = 'unvisited'
+            // Remove the node from the nodeWall array
+            this.nodeWall.splice(nodeInWallArray, 1)
+        }
+    } else if (before === 'unvisited' && after === 'visited'){
+        // Change the class name of the node in html to be a wall
+        htmlNode.className = 'visited'
+        // Change the status of the node in nodeArray to be a wall
+        node.status = 'visited'
+        // Add it to the nodeVisited array
+        this.nodeVisited.push(node)
+    } else if (before === 'visited' && after === 'unvisited'){
+        // If the node exists
+        if (nodeInVisitedArray !== -1) {
+            // Change the class name of the node in html to be unvisited
+            htmlNode.className = 'unvisited'
+            // Change the status of the node in nodeArray to be unvisited
+            node.status = 'unvisited'
+            // Remove the node from the nodevisited array
+            this.nodeVisited.splice(nodeInVisitedArray, 1)
+        }
+    } else if (before === 'visited' && after === 'path'){
+        // Change the class name of the node in html to be a path
+        htmlNode.className = 'path'
+        // Change the status of the node in nodeArray to be a path
+        node.status = 'path'
+    } else if (before === 'path' && after === 'visited'){
+        // If the node exists
+        if (nodeInVisitedArray !== -1) {
+            // Change the class name of the node in html to be unvisited
+            htmlNode.className = 'unvisited'
+            // Change the status of the node in nodeArray to be unvisited
+            node.status = 'unvisited'
         }
     }
 }
 
-Board.prototype.setStart = function(x, y) {
-     // Get location of new node
-    node = document.getElementById(x + ',' + y)
-    // If the new location is a wall
-    if ($(node).attr('class') === 'wall') {
-        // Change it to a start node
-        $(node).addClass('start').removeClass('wall')
-    // If the new location is the end node
-    } else if ($(node).attr('class') === 'end') {
-        // End the function
-        return
-    // If the new location is an unvisited node
-    } else if ($(node).attr('class') === 'unvisited') {
-        // Change it to a start node
-        $(node).addClass('start').removeClass('unvisited')
-    }
-    // Erase previous start node location
-    if (this.isStartDragging === true || this.isReset === true) {
-        previousNode = document.getElementById(this.startCoordinate)
-        if (previousNode === node) {
-            return
-        } else {
-            $(previousNode).addClass('unvisited').removeClass('start')
-        }
-        
-    } 
-    this.nodeArray[x][y].status = 'start'
-    this.startCoordinate = x + ',' + y
-    
-}
-
-Board.prototype.setEnd = function(x, y) {
+Board.prototype.setSpecial = function(x, y, option) {
     // Get location of new node
     node = document.getElementById(x + ',' + y)
+    // Get the counter option
+    counterOption = ''
+    // Get location of previous node
+    previousNode = ''
+    if (option === 'start') {
+        // Get the previous node location
+        previousNode = document.getElementById(this.startCoordinate)
+        counterOption = 'end'
+    } else {
+        // Get the previous node location
+        previousNode = document.getElementById(this.endCoordinate)
+        counterOption = 'start'
+    }
     // If the new location is a wall
-    if ($(node).attr('class') === 'wall') {
-        // Change it to a end node
-        $(node).addClass('end').removeClass('wall')
-    // If the new location is the start node
-    } else if ($(node).attr('class') === 'start') {
+    if (node.className === 'wall' || node.className === 'unvisited') {
+        // Change the class name to be the option
+        node.className = option
+        // Set the status of the node to be the option
+        this.nodeArray[x][y].status = option
+    // If the new location is the counter option
+    } else if (node.className === counterOption) {
         // End the function
         return
-    // If the new location is an unvisited node
-    } else if ($(node).attr('class') === 'unvisited') {
-        // Change it to a end node
-        $(node).addClass('end').removeClass('unvisited')
-    }
-    // Erase previous end node location
-    if (this.isEndDragging === true || this.isReset === true) {
-        previousNode = document.getElementById(this.endCoordinate)
-        if (previousNode === node) {
-            return
-        } else {
-            $(previousNode).addClass('unvisited').removeClass('end')
-        }
     } 
-    this.nodeArray[x][y].status = 'end'
-    this.endCoordinate = x + ',' + y
-}
 
+    // Update new node status
+    this.nodeArray[x][y].status = option
+
+    // Erase previous node location if user is dragging a node or if user resets
+    if (this.isStartDragging === true || this.isEndDragging === true || this.isReset === true) {
+        // If the node is not in the same place
+        if (previousNode !== node) {
+            // Set the previous node in html to be unvisited
+            previousNode.className = 'unvisited'
+            // Set the previous node in the node array to be unvisited
+            this.nodeArray[previousNode.id.split(',')[0]][previousNode.id.split(',')[1]].status = 'unvisited' 
+        }
+    }
+    if (option === 'start') {
+        // Update start coordinate
+        this.startCoordinate = x + ',' + y
+    } else {
+        // Update end coordinate
+        this.endCoordinate = x + ',' + y
+    }
+}
 
 Board.prototype.clear = function() {
     let numberOfWallNodes = this.nodeWall.length
     // Go through all nodes that are currently walls
     for (i = 0; i < numberOfWallNodes; i++) {
+        // Remove the node from the nodeWall array
         currentNode = this.nodeWall.pop()
-        $(currentNode).addClass('unvisited').removeClass('wall')
+        // Change the class name of the node in html to be a unvisited
+        document.getElementById(currentNode.x + ',' + currentNode.y).className = 'unvisited'
+        // Change the status of the node in nodeArray to be a unvisited
+        currentNode.status = 'unvisited'
+    }
+
+    let numberOfVisitedNodes = this.nodeVisited.length
+    // Go through all nodes that are currently walls
+    for (i = 0; i < numberOfVisitedNodes; i++) {
+        // Remove the node from the nodeWall array
+        currentNode = this.nodeVisited.pop()
+        // Change the class name of the node in html to be a unvisited
+        document.getElementById(currentNode.x + ',' + currentNode.y).className = 'unvisited'
+        // Change the status of the node in nodeArray to be a unvisited
+        currentNode.status = 'unvisited'
     }
 }
 
 // Queue implemented using push() and shift() javascript functions
 Board.prototype.breadthFirstTraversal = function() {
+    function backtrace (self, endNode) {
+        finalPath = []
+        // Push the end node onto the finalPath
+        finalPath.push(endNode)
+        while (endNode.parent != null) {
+            self.setNormal(endNode.x, endNode.y, 'visited', 'path')
+            finalPath.push(endNode.parent)
+            endNode = endNode.parent
+        }
+        return finalPath
+    }
+    // Create a queue to implement the traversal
     queue = []
-    x = this.startCoordinate.split(',')[0]
-    y = this.startCoordinate.split(',')[1]
-    queue.push(this.nodeArray[x][y])
-    console.log(queue)
+    // Create a queue to hold the various paths to the end
+    queueFinalPath = []
+    dr = [1, -1, 0, 0]
+    dc = [0, 0, -1, 1]
+    xStart = this.startCoordinate.split(',')[0]
+    yStart = this.startCoordinate.split(',')[1]
+    xEnd = this.endCoordinate.split(',')[0]
+    yEnd = this.endCoordinate.split(',')[1]
+
+    // Enqueue the starting node
+    queue.push(this.nodeArray[xStart][yStart])
+
+    // Mark the start as visited
+    this.setNormal(xStart, yStart, 'unvisited', 'visited')
+
+    // While the queue is not empty
+    while (queue.length !== 0) {
+        // Dequeue the currentNode
+        currentNode = queue.shift()
+        for (i = 0; i < 4; i++){
+            // Get a neighbor node
+            if (currentNode.x + dr[i] >= 0 && currentNode.x + dr[i] <= height && currentNode.y + dc[i] >= 0 && currentNode.y + dc[i] <= width){
+                nextNode = this.nodeArray[currentNode.x + dr[i]][currentNode.y + dc[i]]
+                // If the neighbor node is the end node
+                if (nextNode === this.nodeArray[xEnd][yEnd]){
+                    // Set the neighbor node's parent as the current node
+                    nextNode.parent = currentNode
+                    backtrace(this, nextNode)
+                    return
+                // If the neighbor node is not visited yet
+                } else if (nextNode.status === 'unvisited') {
+                    // Add the neighbor node to the queue
+                    queue.push(nextNode)
+                    // Set the neighbor node's parent as the current node
+                    nextNode.parent = currentNode
+                    // Mark as visited
+                    this.setNormal(nextNode.x, nextNode.y, 'unvisited', 'visited')
+                }
+            }
+        }
+    }
 }
 
 
 
-let titleHeight = document.getElementById("title").clientHeight
-let documentHeight = $(window).height()
-let documentWidth = $(window).width()
-let height = (documentHeight - titleHeight)/32
+let titleHeight = document.getElementById('header').clientHeight
+let documentHeight = window.innerHeight
+let documentWidth = window.innerWidth
+let height = (documentHeight - titleHeight)/30
 let width = documentWidth/25
 
 let board = new Board()
